da9340bfbb0857fe699ffa1b82601842ecac4a06
Greg Bowler <greg.bowler@g105b.com>
2015-10-28 16:34:17 +0000

    Add base editor and style

--- src/jotdown.js
+++ src/jotdown.js
@@ -4,4 +4,43 @@ var
 	activeEditor = null,
 $$;
 
+(function go() {
+	var
+		textareaList = document.querySelectorAll("textarea[data-jotdown]"),
+	$$;
+
+	[].forEach.call(textareaList, convertTextarea);
+})();
+
+function convertTextarea(textarea) {
+	var 
+		jotdownElement = createJotdown(textarea),
+	$$;
+
+	// Create part two of the two-way element link.
+	textarea.jotdownElement = jotdownElement;
+}
+
+function createJotdown(textarea) {
+	var
+		jotdownEditor = document.createElement("jotdown-editor"),
+		jotdownToolbar = document.createElement("jotdown-toolbar"),
+		range,
+		selection,
+	$$;
+
+	// Create part one of the two-way element link.
+	jotdownEditor.textarea = textarea;
+	textarea.parentNode.insertBefore(jotdownEditor, textarea);
+
+	jotdownEditor.innerHTML = textarea.value;
+	jotdownEditor.setAttribute("contenteditable", true);
+
+	if(textarea.hasAttribute("autofocus")) {
+		jotdownEditor.focus();
+	}
+
+	return jotdownEditor;
+}
+
 });//#
